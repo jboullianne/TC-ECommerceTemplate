b5cdf07556e4ecf11c19bf5593ed2a6e4d917418
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-12-02 20:17:05 -0500

    Initial Commit

---
+++ EcommerceTemplate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EcommerceTemplate
+//
+//  Created by Jean-Marc Boullianne on 12/2/19.
+//  Copyright © 2019 Jean-Marc Boullianne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
